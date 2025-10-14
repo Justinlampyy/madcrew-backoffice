a629a403fa7ccb5d7de5743fe7cdc531a6090849
Justinlampy <justinlamberink@gmail.com>
2025-10-14 23:30:06 +0200

    update page

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,14 +1,10 @@
+'use client'
+import OrdersView from '@/components/OrdersView'
+
 export const dynamic = 'force-dynamic'
 export const revalidate = 0
 export const fetchCache = 'force-no-store'
 
-import OrdersView from '@/components/OrdersView'
-import ClientErrorBoundary from '@/components/ClientErrorBoundary'
-
-export default function AdminPage() {
-  return (
-    <ClientErrorBoundary>
-      <OrdersView />
-    </ClientErrorBoundary>
-  )
+export default function AdminDashboard() {
+  return <OrdersView />
 }
