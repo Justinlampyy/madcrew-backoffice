47e1b449fdc25d90ee3215937f411b38c51535bc
Justinlampy <justinlamberink@gmail.com>
2025-10-14 23:33:38 +0200

    page fix
    Fix (make the page a Server Component again)

--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -1,9 +1,8 @@
-'use client'
-import OrdersView from '@/components/OrdersView'
-
 export const dynamic = 'force-dynamic'
 export const revalidate = 0
 export const fetchCache = 'force-no-store'
+
+import OrdersView from '@/components/OrdersView'
 
 export default function OrdersPage() {
   return <OrdersView />
